c20c7b935d347ce7c3c083519c10a05e06011665
AmadeoNeto <amadeotatocota@gmail.com>
2022-10-19 15:28:44 -0300

    Dotnet project and code refactor
    Co-authored-by: Artur Leal <arturlealbs@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-using System;
+﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
-public class QLearning{
+public class Program{
 
     public static void PrintMatrix(double[,] matrix){
+        string printValues = "Matriz Q: \n\n";
         for (int i=0; i < 6; i++){
-            Console.Write("{ ");
-            Console.Write("{0:f} {1:f} {2:f} {3:f}", 
+            printValues += "{ ";
+            printValues += String.Format("{0:f} {1:f} {2:f} {3:f}", 
                               matrix[i,0], matrix[i,1],matrix[i,2],matrix[i,3]);
-            Console.WriteLine(" }");
-        }    
+            printValues += " }\n";
+        }
+        Console.WriteLine(printValues);
+    }
+
+    public static void PrintPolicy(string[] policy){
+        Console.WriteLine("Politica: \n\n" +
+                           policy[4] + " "+ "1\n" + 
+                           policy[2] + " "+ policy[3] +"\n" +
+                           policy[0] + " "+ policy[1]);
     }
 
     public static double GetBestAction(double [,] qMatrix, int nextState){
@@ -22,11 +32,6 @@ public class QLearning{
         }
         return qBestAction;
     } 
-    public static void UpdateState(double [,] qMatrix, int state, int nextState, int action, double alpha, double gamma, int[,] rwMatrix){
-        double qEstimated = rwMatrix[state,nextState] + gamma * GetBestAction(qMatrix,nextState);
-        qMatrix[state,action] = qMatrix[state,action] + alpha*(qEstimated-qMatrix[state,action]);
-    }
-
     public static string [] GetPolicy(double [,] qMatrix){
         string [] actions_names = new string [] {"UP","DW", "LF", "RG"};
         string [] policy = new string [6];
@@ -42,6 +47,15 @@ public class QLearning{
             policy[i] = actions_names[currentIndex];
         }
         return policy;
+    }
+    public static void UpdateState(double [,] qMatrix, int state, int nextState, int action, double alpha, double gamma, int[,] rwMatrix){
+        double qEstimated = rwMatrix[state,nextState] + gamma * GetBestAction(qMatrix,nextState);
+        qMatrix[state,action] = qMatrix[state,action] + alpha*(qEstimated-qMatrix[state,action]);
+    }
+    public static void RunThroughTrajectory(double[,] qMatrix, int [,] trajectory, double alpha, double gamma,int [,] rwMatrix ){
+        for (int i =0; i < trajectory.GetLength(0); i++){
+            UpdateState(qMatrix, trajectory[i,0] - 1,trajectory[i,2] - 1,trajectory[i,1] - 1,alpha, gamma, rwMatrix);
+        }
     }
     
     public static void Main(string[] args){
@@ -75,29 +89,17 @@ public class QLearning{
         int [,] trajectory3 = {{1,2,1},{1,1,3},{3,4,5},{5,1,5},{5,2,5},{5,3,5},{5,2,3},{3,1,5},{5,4,5},{5,4,6}};
         int [,] trajectory4 = {{1,1,3},{3,4,4},{4,1,6}};
         int [,] trajectory5 = {{1,3,1},{1,1,1},{1,1,3},{3,1,5},{5,3,5},{5,2,3},{3,1,5},{5,1,5},{5,1,5},{5,2,3},{3,1,3},{3,3,3},{3,4,4},{4,1,6}};
-        
+
         //Executando o Q Learning Sequencialmente pelas 5 trajetórias
-        for(int i=0; i < 9; i++){
-            UpdateState(qMatrix, trajectory1[i,0] - 1,trajectory1[i,2] - 1,trajectory1[i,1] - 1,alpha, gamma, rwMatrix);
-        }
-        for(int i=0; i < 6; i++){
-            UpdateState(qMatrix, trajectory2[i,0] - 1,trajectory2[i,2] - 1,trajectory2[i,1] - 1,alpha, gamma, rwMatrix);
-        }
-        for(int i=0; i < 10; i++){
-            UpdateState(qMatrix, trajectory3[i,0] - 1,trajectory3[i,2] - 1,trajectory3[i,1] - 1,alpha, gamma, rwMatrix);
-        }
-        for(int i=0; i < 3; i++){
-            UpdateState(qMatrix, trajectory4[i,0] - 1,trajectory4[i,2] - 1,trajectory4[i,1] - 1,alpha, gamma, rwMatrix);
-        }
-        for(int i=0; i < 14; i++){
-            UpdateState(qMatrix, trajectory5[i,0] - 1,trajectory5[i,2] - 1,trajectory5[i,1] - 1,alpha, gamma, rwMatrix);
-        }
-        
+        RunThroughTrajectory(qMatrix, trajectory1, alpha, gamma, rwMatrix);
+        RunThroughTrajectory(qMatrix, trajectory2, alpha, gamma, rwMatrix);
+        RunThroughTrajectory(qMatrix, trajectory3, alpha, gamma, rwMatrix);
+        RunThroughTrajectory(qMatrix, trajectory4, alpha, gamma, rwMatrix);
+        RunThroughTrajectory(qMatrix, trajectory5, alpha, gamma, rwMatrix);
+    
         //Imprimindo a Matriz Q resultante das operações e a política
         string [] policy = GetPolicy(qMatrix);
         PrintMatrix(qMatrix);
-        Console.WriteLine(policy[4] + " "+ "1");
-        Console.WriteLine(policy[2] + " "+ policy[3]);
-        Console.WriteLine(policy[0] + " "+ policy[1]);
+        PrintPolicy(policy);
     }
 }
